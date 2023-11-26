b8ea82d3292581c6a8be17f680addb4757966373
Kody Simpson <kodysimpson0616@gmail.com>
2023-11-25 20:27:25 -0600

    stop movement on pause

--- Infinite Feather/Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
+++ Infinite Feather/Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using System.Collections;
 
-namespace AstronautPlayer
+namespace Stylized_Astronaut.Character
 {
 
 	public class AstronautPlayer : MonoBehaviour {
 
-		private Animator anim;
-		private CharacterController controller;
+		private Animator _anim;
+		private CharacterController _controller;
 
-		private CameraSwitcher cameraSwitcher;
+		private CameraSwitcher _cameraSwitcher;
 
 		public float speed = 600.0f;
 		public float turnSpeed = 400.0f;
@@ -20,64 +19,69 @@ namespace AstronautPlayer
         
         //debugging
         public bool isOnGround;
+        private static readonly int AnimationPar = Animator.StringToHash("AnimationPar");
 
         void Start () {
-			if (cameraSwitcher == null)
+			if (_cameraSwitcher == null)
 			{
-				cameraSwitcher = FindObjectOfType<CameraSwitcher>();
+				_cameraSwitcher = FindObjectOfType<CameraSwitcher>();
 			}
-			controller = GetComponent <CharacterController>();
-			anim = gameObject.GetComponentInChildren<Animator>();
+			_controller = GetComponent <CharacterController>();
+			_anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
         void Update (){
     
+	        if (PauseManager.IsPaused)
+	        {
+		        return;
+	        }
+	        
 	        //If the player is on the ground, reset the velocity
-	        if (controller.isGrounded && velocity.y < 0)
+	        if (_controller.isGrounded && velocity.y < 0)
 	        {
 		        velocity.y = -2f;
 	        }
 
-	        isOnGround = controller.isGrounded;  // Update debug variable
+	        isOnGround = _controller.isGrounded;  // Update debug variable
     
 	        Vector3 moveDirection = Vector3.zero;
 			float horizontal = Input.GetAxis("Horizontal");
 			float vertical = Input.GetAxis("Vertical");
 			moveDirection = transform.forward * vertical;
 
-            if (cameraSwitcher.isFirstPerson)
+            if (_cameraSwitcher.isFirstPerson)
             {
-				moveDirection = moveDirection + (transform.right * horizontal);
+				moveDirection += (transform.right * horizontal);
             }
 
             //apply the speed onto the movmenet
-            moveDirection = moveDirection * speed;
+            moveDirection *= speed;
     
 	        // Combine vertical and gravity movement
 	        velocity.y += gravity * Time.deltaTime;
 	        moveDirection.y = velocity.y;
     
-	        controller.Move(moveDirection * Time.deltaTime);  // Single move call
+	        _controller.Move(moveDirection * Time.deltaTime);
 
 	        // Check if grounded right after the move call to update status for the next frame
-	        isOnGround = controller.isGrounded;  
+	        isOnGround = _controller.isGrounded;  
     
 	        if (Input.GetKey("w")) {
-		        anim.SetInteger("AnimationPar", 1);
+		        _anim.SetInteger(AnimationPar, 1);
 	        } else {
-		        anim.SetInteger("AnimationPar", 0);
+		        _anim.SetInteger(AnimationPar, 0);
 	        }
 
-			//only if the player is in third person, rotate when there is horozontal movement
-			if (!cameraSwitcher.isFirstPerson)
+			//only if the player is in third person, rotate when there is horizontal movement
+			if (!_cameraSwitcher.isFirstPerson)
 			{
                 transform.Rotate(0, horizontal * turnSpeed * Time.deltaTime, 0);
             }
 
 	        // Perform jump after the move to ensure isGrounded is updated
-	        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+	        if (Input.GetButtonDown("Jump") && _controller.isGrounded && !PauseManager.IsPaused)
 	        {
-		        print("JUMPED!!!");
 		        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 	        }
         }
